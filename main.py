2235f898e3c192cb34900f63ec7d7df8bbc3d207
Brian <brianc.boston@gmail.com>
2022-05-23 11:43:18 +0800

    LeetCode day 47

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ from LeetFunctions import *
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    str1 = ""
-    total = 0
-    for i in range (2000,3000):
-        num = str(i)
-        total += num.count('2')
-        str1 += num
-    print(total)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    str1 = "dbcba"
+    str2 = "cccccccc"
+    print(max(str1, str2))
