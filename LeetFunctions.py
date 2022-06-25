8d66e7f1d6fa6a51e879c66b69a72a086abea646
BrianChen0527 <brianc.boston@gmail.com>
2022-06-25 21:50:54 +0800

    LeetCode day 50

--- LeetFunctions.py
+++ LeetFunctions.py
@@ -84,7 +84,7 @@ class Interval:
 
 
 # Given a list of intervals, merge all the overlapping intervals to produce a list that has only mutually exclusive
-# intervals. 
+# intervals.
 def merge(intervals):
     mergedIntervals = []
 
