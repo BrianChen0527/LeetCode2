7876eccd2b1eacee9f036a994c688b303d82b454
BrianChen0527 <brianc.boston@gmail.com>
2022-07-24 11:26:58 +0800

    Leetcode day 63

--- LeetFunctions.py
+++ LeetFunctions.py
@@ -788,6 +788,17 @@ def exist(self, board: List[List[str]], word: str) -> bool:
     return False
 
 
+# https://leetcode.com/problems/coin-change/submissions/
+def coinChange(self, coins: List[int], amount: int) -> int:
+    dp = [math.inf] * amount
+    dp[0] = 1
+
+    for i in range(amount):
+        for c in coins:
+            if i - c >= 0 and dp[i - c] != math.inf:
+                dp[i] = min(dp[i], dp[i - c] + 1)
+    return dp[-1] if dp[-1] != math.inf else -1
+
 
 
 
