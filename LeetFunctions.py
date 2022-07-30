2b5fff40662d375d64b98ba6d1781fdb211066e0
BrianChen0527 <brianc.boston@gmail.com>
2022-07-30 11:06:47 +0800

    pushing P

--- LeetFunctions.py
+++ LeetFunctions.py
@@ -1106,14 +1106,17 @@ def letterCombinations(digits: str) -> List[str]:
     def genPerms(path, remaining):
         if not remaining:
             allPerms.append(path)
+            return
         for c in d[remaining[0]]:
             genPerms(path + c, remaining[1:])
 
-
+    if not digits:
+        return []
     d = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
     allPerms = []
-    genPerms([], digits)
+    genPerms("", digits)
     return allPerms
+
 
 class TimeMap:
 
