25d33026a19bd4764a8eb4218c471ffc876ee00b
Brian Ying-Chi Chen <41340902+BrianChen0527@users.noreply.github.com>
2022-08-01 15:37:15 +0800

    LeetCode day 69

--- LeetFunctions.py
+++ LeetFunctions.py
@@ -1202,6 +1202,9 @@ class LRUCache:
             self.cache.popitem(last=False)
 
 
+ def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        return heapq.nsmallest(k, points, key=lambda x: x[0]**2 + x[1]**2)
+
 
 class TimeMap:
 
