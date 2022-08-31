9364ad952a8933bf568eb46cee323337cf32921e
BrianChen0527 <brianc.boston@gmail.com>
2022-08-30 21:46:31 -0400

    LeetCode day 87

--- LeetFunctions.py
+++ LeetFunctions.py
@@ -2047,6 +2047,30 @@ def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[Li
     return head.next
 
 
+# https://leetcode.com/problems/all-ancestors-of-a-node-in-a-directed-acyclic-graph/submissions/
+def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+    def childrenDFS(node):
+        if node in visited:
+            return visited[node]
+
+        children = set()
+        for v in G[node]:
+            children.add(v)
+            children = children | childrenDFS(v)
+
+        visited[node] = children
+        return children
+
+    G = collections.defaultdict(list)
+    for e in edges:
+        G[e[1]].append(e[0])
+
+    ans, visited = [[] for _ in range(n)], {}
+    for i in range(n):
+        ans[i] = sorted(list(childrenDFS(i)))
+    return ans
+
+
 # https://leetcode.com/problems/generate-parentheses/
 def generateParenthesis(n: int) -> List[str]:
     def parenthesisGenerator(k, permutation):
@@ -2358,3 +2382,42 @@ class MedianFinder:
             return (self.large[0] - self.small[0]) / 2
         else:
             return -float(self.small[0])
+
+
+class customNode:
+    def __init__(self, val: int, dist: int):
+        self.val = val
+        self.dist = dist
+        self.neighbors = []
+
+
+def shortestDist1(start: customNode, end: customNode):
+    currTree = set()
+
+    canReach = [(0, start)]
+    while canReach:
+        currDist, currNode = heapq.heappop(canReach)
+        if currNode.val in currTree:
+            continue
+        if currNode == end:
+            return currDist
+
+        currTree.add(currNode.val)
+        for n in currNode.neighbors:
+            heapq.heappush(canReach, (currDist + n.dist, n))
+
+
+def shortestDist2(start: customNode, end: customNode, nodesGraph: dict):
+    currTree = set()
+
+    canReach = [(0, start)]
+    while canReach:
+        currDist, currNode = heapq.heappop(canReach)
+        if currNode in currTree:
+            continue
+        if currNode == end:
+            return currDist
+
+        currTree.add(currNode)
+        for n in nodesGraph[currNode]:
+            heapq.heappush(canReach, (currDist + n[1], n[0]))
