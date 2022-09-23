46faa27f9b35eb3f5996f5b131bfba3f4d975b81
BrianChen0527 <brianc.boston@gmail.com>
2022-09-22 23:01:00 -0400

    Got into AMAZON!

---
+++ eecs445tests.py
@@ -0,0 +1,53 @@
+import string
+import numpy as np
+
+def extract_word(input_string):
+
+    # TODO: Implement this function
+    for c in string.punctuation:
+        input_string = input_string.replace(c, ' ')
+    return list(filter(None, input_string.lower().split(' ')))
+
+def extract_dictionary(df):
+    word_dict = {}
+    # TODO: Implement this function
+    idx = 0
+    for i in df.index:
+        words = extract_word(df['text'][i])
+        for w in words:
+            if w in word_dict:
+                continue
+            word_dict[w] = idx
+            idx += 1
+    return word_dict
+
+
+def generate_feature_matrix(df, word_dict):
+    """Create matrix of feature vectors for dataset.
+
+    Reads a dataframe and the dictionary of unique words to generate a matrix
+    of {1, 0} feature vectors for each review.  Use the word_dict to find the
+    correct index to set to 1 for each place in the feature vector. The
+    resulting feature matrix should be of dimension (# of reviews, # of words
+    in dictionary).
+
+    Input:
+        df: dataframe that has the text and labels
+        word_dict: dictionary of words mapping to indices
+    Returns:
+        a numpy matrix of dimension (# of reviews, # of words in dictionary)
+    """
+    number_of_reviews = df.shape[0]
+    number_of_words = len(word_dict)
+    feature_matrix = np.zeros((number_of_reviews, number_of_words))
+    # TODO: Implement this function
+
+    for i in range(number_of_reviews):
+        words = extract_word(df['text'][i])
+        for w in words:
+            feature_matrix[i][word_dict[w]] = 1
+
+    return feature_matrix
+
+
+
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import collections
 from LeetFunctions import *
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from eecs445tests import *
 import sys
 import re
 # import numpy as np
@@ -15,12 +15,20 @@ from LeetFunctions import WordDictionary
 import random
 
 import numpy as np
-
+import pandas as pd
 
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    s = "I love EECS 445. It's my favorite course!"
-    print(extract_word(s))
+    df = pd.read_csv("dataset.csv")
+    word_dict = extract_dictionary(df)
+    arr = generate_feature_matrix(df, word_dict)
+    print(word_dict)
+
+
+
+
+
+
