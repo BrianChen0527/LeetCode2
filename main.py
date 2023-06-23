95dcabc124c2da3fc67b7c9eb5bc062a3310a65d
Brian Ying-Chi Chen <brianc.boston@gmail.com>
2023-06-22 22:14:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -21,9 +21,14 @@ from LeetFunctions import *
 
 
 def main():
-    arr1 = [7,2,4]
-    print(maxSlidingWindow(arr1, 2))
-
+    arr1 = ["bb","bababab","baab","abaabaa","aaba","","bbaa","aba","baa","b"]
+    word = "abcddcba"
+    p = PalindromePairs
+    print(p.palindromePairs(p, arr1))
+    n = len(word) // 2
+    print(word[:n])
+    print(word[n + (len(word) % 2):][::-1])
+    print(p.isPalindrome(p, "baab"))
 
 if __name__ == '__main__':
     main()
