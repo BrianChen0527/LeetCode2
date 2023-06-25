59eb90eda1942efc914ac48b76ec007487f28881
Brian Ying-Chi Chen <brianc.boston@gmail.com>
2023-06-25 12:34:25 -0700

    LeetCode grind day 140!

--- main.py
+++ main.py
@@ -21,14 +21,20 @@ from LeetFunctions import *
 
 
 def main():
-    arr1 = ["bb","bababab","baab","abaabaa","aaba","","bbaa","aba","baa","b"]
-    word = "abcddcba"
-    p = PalindromePairs
-    print(p.palindromePairs(p, arr1))
-    n = len(word) // 2
-    print(word[:n])
-    print(word[n + (len(word) % 2):][::-1])
-    print(p.isPalindrome(p, "baab"))
+    l1 = ListNode(1)
+    l2 = ListNode(2)
+    l3 = ListNode(3)
+    l4 = ListNode(4)
+    l5 = ListNode(5)
+    l1.next = l2
+    l2.next = l3
+    l3.next = l4
+    l4.next = l5
+
+    l6 = reverseKGroup(l1,3)
+    while l6:
+        print(l6.val)
+        l6 = l6.next
 
 if __name__ == '__main__':
     main()
