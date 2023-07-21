bb957392d812998fcaa8df9c03080a6f4b2b9797
Brian Ying-Chi Chen <brianc.boston@gmail.com>
2023-07-20 22:42:33 -0700

    Leetcode grind day 156!

--- main.py
+++ main.py
@@ -21,9 +21,8 @@ from LeetFunctions import *
 
 
 def main():
-    arr = [1, 7, 4, 6, 9]
-    print(arr)
-    print(prevPermOpt1(arr))
+    roads = [[0,6,7],[0,1,2],[1,2,3],[1,3,3],[6,3,3],[3,5,1],[6,5,1],[2,5,1],[0,4,5],[4,6,2]]
+    countPaths(7, roads)
 
 if __name__ == '__main__':
     main()
