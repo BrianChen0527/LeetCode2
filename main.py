0ff6a819dbe876b99233280d1d2d30053ba9c1bf
Brian Ying-Chi Chen <brianc.boston@gmail.com>
2023-08-06 11:03:02 +0800

    LeetCode grind 157!

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ from LeetFunctions import *
 
 
 def main():
-    roads = [[0,6,7],[0,1,2],[1,2,3],[1,3,3],[6,3,3],[3,5,1],[6,5,1],[2,5,1],[0,4,5],[4,6,2]]
-    countPaths(7, roads)
+    arr = [-1,3,5,1,4,2,-9]
+    print(maxNonOverlapping(arr, 6))
 
 if __name__ == '__main__':
     main()
